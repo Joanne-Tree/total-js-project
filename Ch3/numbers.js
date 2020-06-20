db4379b0b80ec2e9fc5c93adee7d963f6d42f250
unknown <tree.joanne@gmail.com>
2020-06-20 11:08:03 +0100

    Added Alert Button

---
+++ Ch3/numbers.js
@@ -0,0 +1,6 @@
+function numberAlert(){
+	var firstNumber = prompt("Enter the first number");
+	var secondNumber = prompt("Enter the second number");
+	var total = Number(firstNumber)	+Number(secondNumber);
+	alert (total);
+}
