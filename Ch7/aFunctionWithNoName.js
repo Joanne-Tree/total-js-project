3a1062c6a79a6dc39d1592d7a1060ff1862e2f67
unknown <tree.joanne@gmail.com>
2020-07-18 12:07:02 +0100

    updated function layouts

--- Ch7/aFunctionWithNoName.js
+++ Ch7/aFunctionWithNoName.js
@@ -1,3 +1,20 @@
 var doTheThing = function(thingToDo) {
 	document.write("I will do this thing: " + thingToDo);
 }
+
+var doTheThing2 = (thingToDo2) => 
+	(document.write("I will do this thing: " + thingToDo2));
+
+	doTheThing("thingToDo");
+	
+	
+	doTheThing2("thingToDo2");
+
+
+var doTheThing3 = () => 
+	"I will do this thing: ";
+
+console.log(doTheThing3());
+
+var cake = "Red " + doTheThing3();
+console.log(cake);
